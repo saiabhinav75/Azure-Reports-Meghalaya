222dade40c6a7ce336ed382dee86aafbd3eca761
arpit017 <shuklaji9706@gmail.com>
2024-07-31 15:57:41 +0530

    changes made

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,25 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.raw({ type: 'application/octet-stream', limit: '50mb' }));
-app.use(cors()); 
+
+
+
+const whitelist: string[] = ['http://localhost:8081','http://localhost:3000','https://staging.d2zqkvxnnybywm.amplifyapp.com'];
+
+const corsOptions = {
+  origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
+    if (origin && whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+
+app.use(cors(corsOptions));
+
+
+
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   res.setHeader('Access-Control-Allow-Origin', '*');
