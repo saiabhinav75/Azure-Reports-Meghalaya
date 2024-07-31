f82d65e9d321ae46bc6dea08a337d2ff4bad7a38
arpit017 <shuklaji9706@gmail.com>
2024-07-31 15:54:41 +0530

    changes made

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json({ limit: '50mb' }));
 app.use(express.raw({ type: 'application/octet-stream', limit: '50mb' }));
-// app.use(cors()); 
+app.use(cors()); 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   res.setHeader('Access-Control-Allow-Origin', '*');
